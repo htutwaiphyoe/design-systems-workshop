5c97d9bf2d5fc882f0888e1c0fbf840e30a60c8d
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-10-06 21:39:26 +0630

    NPM Modules

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const validator = require("validator");
-
+const chalk = require("chalk");
 console.log(validator.isEmail("foo@bar.com"));
 console.log(validator.isURL("http://foo.com"));
+
+console.log(chalk.bgGreen("Success!"));
